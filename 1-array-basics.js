1496d8507b6526e93999e6cb7837f923d3260bc8
Sbklopp <sbklopp11@gmail.com>
2023-02-06 10:01:04 -0500

    added array and loops

---
+++ 1-array-basics.js
@@ -0,0 +1,22 @@
+
+//an array is a type of variable,
+//arrays can contain strings, integers, bools, objects and even other arrays.
+let scores = [92, 79, 64, 97, 58, 83, 49, 73, 90];
+
+
+
+// we can find the size of the array by using the .length method
+// console.log(scores.length);
+
+// we can access individual members of an array
+// by calling the arrays name (scores) followed 
+// by [] with index inside of the square brackets
+// **NOTE the first member of the array will be index 0**
+
+const firstScore = scores[0];
+const secondScore = scores[1];
+const thirdScore = scores[2]
+
+// console.log(firstScore)
+
+console.log(scores[3])
---
+++ 2-loops-basics.js
@@ -0,0 +1,34 @@
+
+let scores = [92, 79, 64, 97, 58, 83, 49, 73, 90];
+
+let newScores = []
+
+let aGrades = []
+
+
+
+
+// for (let i=0; i<10; i++) {
+//     // console.log(i)
+// }
+
+// we use a loop to add 3 to each score 
+for (let i = 0; i < scores.length; i++){
+    newScores.push(scores[i] + 3)
+}
+
+// we loops to find the scores that are greater than 90
+for (let i = 0; i < scores.length; i++){
+    if (scores[i] >= 90){
+        aGrades.push(scores[i])
+    }
+}
+
+// console.log(newScores)
+
+// console.log(aGrades)
+
+// finds scores over 90 using filter
+let goodGrades = scores.filter(score => score > 89)
+
+// console.log(goodGrades)
