a93757382162643adb4041551822b70700381c60
Sbklopp <sbklopp11@gmail.com>
2023-02-13 11:02:07 -0500

    es6

---
+++ 5-ternary.js
@@ -0,0 +1,7 @@
+let grade1 = 72
+let grade2 = 55
+
+//                             if statement 
+const didTheyPass = (score) => score > 59 ? true : false 
+
+console.log(didTheyPass(grade2))
---
+++ 6-ES6-functions.js
@@ -0,0 +1,28 @@
+// rewrite the following functions using ES6 notation
+
+
+function sayHello(){
+    console.log('Hello World')
+}
+
+const sayHelloES6 = () => {
+    console.log('Hello World')
+}
+
+
+function doubleTheNumber(num){
+    return num * 2
+}
+
+const doubleTheNumberES6 = (num) => {
+    return num * 2
+}
+
+
+function findTheSum(num1, num2){
+    return num1 + num2
+}
+
+const findTheSumES6 = (num1, num2) => {
+    return num1 + num2
+}
